a83f128412c274b59ca375107fc89192ca948712
Luca Calcaterra <calcaterra.luca@gmail.com>
2022-02-17 18:31:25 +0100

    initial with auth ok

---
+++ client/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Dashboard
+      </h1>
+
+      <p>Hi {{ user.name }}</p>
+      <a href="#" @click.prevent="logout">Logout</a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      user: this.$auth.$state.user
+    }
+  },
+  methods: {
+    async logout () {
+      await this.$auth.logout()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
---
+++ client/pages/login.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-one-third">
+          <h2 class="title has-text-centered">
+            Login
+          </h2>
+
+          <form method="post" @submit.prevent="login">
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control">
+                <input v-model="email" type="email" class="input" required>
+              </div>
+            </div>
+
+            <div class="field">
+              <label class="label">Password</label>
+              <div class="control">
+                <input
+                  v-model="password"
+                  type="password"
+                  class="input"
+                  required
+                >
+              </div>
+            </div>
+
+            <div class="control">
+              <button type="submit" class="button is-dark is-fullwidth">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        const res = await this.$auth.loginWith('laravelSanctum', {
+          data: {
+            email: this.email,
+            password: this.password
+          }
+        })
+        this.$router.push('/')
+        // eslint-disable-next-line no-console
+        console.log(res)
+        console.log(this.$auth)
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
